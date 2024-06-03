e05874d041b8af176d1bdff24f0b52e4b30ee288
zijun0324 <zijunwang324@gmail.com>
2024-06-02 22:17:36 -0700

    Made the camera thingy working

--- my-detection.py
+++ my-detection.py
@@ -5,7 +5,7 @@ import jetson.utils
 import cv2
 import numpy as np
 import time 
-import main, publisher, reciever, file
+import main
 import json
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
@@ -16,6 +16,7 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 vidCap = cv2.VideoCapture("/dev/video0")
 vidCap.set(3, 640)
 vidCap.set(4, 480)
+isDetected = False
 
 while(True):
     ret, frame = vidCap.read()
@@ -30,22 +31,29 @@ while(True):
     detections = net.Detect(cudaImg)
 
     bottle = None
+    #isDetected = False
     number = 0
     for detection in detections:
         bottle = (net.GetClassDesc(detection.ClassID) == 'bottle')
-        if bottle:
+        if bottle and not isDetected:
                 number += 1
+                isDetected = False
                 left, top, right, bottom = int(detection.Left), int(detection.Top), 			int(detection.Right), int(detection.Bottom)
                 cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
                 cv2.imwrite("test.png", frame)
+                print('bottle here')
                 #sending the data
-                #main.addImage("test.png")
-                time.sleep(5)
-    
-    if (number == 0):
-        # error/alert
+                main.addImage("test.png")
+                #time.sleep(2)
+        else: 
+               isDetected = True
+               if isDetected:
+                   print('bottle not here')
+                   main.add_stock("bottle")
 
-         cv2.imshow('OUTPUT', frame)
+               if bottle:
+                   isDetected = False
+    cv2.imshow('OUTPUT', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 
