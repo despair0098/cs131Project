70a634dbf6a3a7de61111229b2b23a7fac1c4b32
zijun0324 <zijunwang324@gmail.com>
2024-06-05 21:35:06 -0700

    Removed the comments from the my-detection.py

--- my-detection.py
+++ my-detection.py
@@ -1,27 +1,15 @@
-#from google.cloud import storage
-#from firebase_admin import credentials, db
 import jetson.inference
 import jetson.utils
 import cv2
 import main
 import numpy as np
 import time 
-#import main
-#import json
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-
-#camera = jetson.utils.videoSource("/dev/video0")      # '/dev/video0' for V4L2
-#display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
 vidCap = cv2.VideoCapture("/dev/video0")
 vidCap.set(3, 640)
 vidCap.set(4, 480)
-#botFlag = False
-#appFlag = False
-#banFlag = False
-#app_printed = False
-#app_not_printed = False
 i = 0
 j = 0
 k = 0
@@ -57,12 +45,8 @@ while(True):
                 left, top, right, bottom = int(detection.Left), int(detection.Top), 			int(detection.Right), int(detection.Bottom)
                 cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
                 cv2.imwrite("bottle.jpeg", frame)
-                #if botFlag == 0:
-                #    break;
                 botFlag = 1
-                #sending the data
-                #main.addImage("bottle.jpeg")
-                #time.sleep(10)
+
         else: 
                 botFlag = 3
 
@@ -73,26 +57,9 @@ while(True):
                 left, top, right, bottom = int(detection.Left), int(detection.Top), 			int(detection.Right), int(detection.Bottom)
                 cv2.rectangle(frame, (left, top), (right, bottom), (255,255,0), 2)
                 cv2.imwrite("apple.jpeg", frame)
-                #if appFlag == 0:
-                #    break;
                 appFlag = 1
-                """
-                if not app_printed:
-                   print("apple here")
-                   app_printed = True
-                break;
-                """
-                #sending the data
-                #main.addImage("apple.jpeg")
-                #time.sleep(10)
         else: 
                 appFlag = 3
-    """
-    if not appFlag:
-       if not app_not_printed:
-          print("apple not here")
-          apple_not_printed = True
-    """
           
     for detection in detections:
         banana = (net.GetClassDesc(detection.ClassID) == 'banana')
@@ -104,15 +71,11 @@ while(True):
                 if banFlag == 0:
                     break;
                 banFlag = 1
-                #sending the data
-                #main.addImage("banana.jpeg")
-                #time.sleep(10)
         else: 
                 banFlag = 3
 
     if botFlag == 1:
         i += 1
-        #print("i is", i)
         if(i == 5 and numBot > 0):
            print("bottle here")
            main.send_signal("bottle",1)
@@ -143,10 +106,9 @@ while(True):
     
     if banFlag == 1:
         k += 1
-        #print("k is", k)
         if(k == 5 and numBan > 0):
            print("banana here")
-       	   main.send_signal("banana",1)
+           main.send_signal("banana",1)
            main.addImage("banana.jpeg")
         banFlag = 0
 
